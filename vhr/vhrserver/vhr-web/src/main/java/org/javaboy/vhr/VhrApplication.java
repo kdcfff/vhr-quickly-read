c9031c3897b16c745c9e4e567b731663d343829f
一路向北 <2480155857@qq.com>
2025-03-12 21:29:02 +0800

    build: 添加 .gitignore 文件并移除 Knife4j 注解- 新增 .gitignore 文件，忽略本地配置和编译生成的文件 - 移除 VhrApplication 中的 @EnableKnife4j 注解

--- vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
@@ -1,6 +1,6 @@
 package org.javaboy.vhr;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @MapperScan(basePackages = "org.javaboy.vhr.mapper")
 @EnableScheduling
-@EnableKnife4j
 public class VhrApplication {
 
     public static void main(String[] args) {
